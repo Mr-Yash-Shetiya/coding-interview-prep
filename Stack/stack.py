e964ea6f1c58ce9485304327b9d7f5c775e64dbe
Mr-Yash-Shetiya <mr.yashshetiya@gmail.com>
2025-08-23 00:12:15 +0530

    Updated stack.py with pop and push

--- Stack/stack.py
+++ Stack/stack.py
@@ -15,7 +15,32 @@ class stack:
             print(temp.value)
             temp = temp.next
 
+    def push(self,value):
+        new = node(value)
+        if self.top is None:
+            self.top = new
+        else:
+            new.next = self.top
+            self.top = new
+        self.height += 1
+
+    def pop(self):
+        if self.top is None:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp.value
+
+
 user_action_stack = stack(1)
+user_action_stack.push(2)
+user_action_stack.push(3)
+print("After Push : ")
+user_action_stack.print_stack()
+user_action_stack.pop()
+print("After Pop : ")
 user_action_stack.print_stack()
 
 
